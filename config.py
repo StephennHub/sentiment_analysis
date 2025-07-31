a2eefe28591c9084b436838f14bc2d8a9dbbddcc
StephennHub <stephenpauljebaraj@gmail.com>
2025-07-31 23:05:53 +0530

    code has upload for sentiment analysis

---
+++ config.py
@@ -0,0 +1,7 @@
+#config file:
+ZIPPEDFILE = "dataset.zip"
+OLDNAME = "training.1600000.processed.noemoticon.csv"
+NEWNAME = "dataset.csv"
+COLUMNNAME = ["target", "ids", "date", "flag", "user", "text" ]
+FILENAME = "trained_model.sav"
+FILEVECTOR = "vectorizer.pkl"
